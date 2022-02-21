1343ba330c0f28790539effa67c9f4bf1f238d23
U02 <403467@naver.com>
2022-02-21 22:28:41 +0900

    220221 파일 업로드 중

--- src/main/java/com/portfolio/lagarto/customer/CustomerController.java
+++ src/main/java/com/portfolio/lagarto/customer/CustomerController.java
@@ -6,9 +6,13 @@ import com.portfolio.lagarto.model.CustomerDto;
 import com.portfolio.lagarto.model.CustomerEntity;
 import com.portfolio.lagarto.model.CustomerVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.lang.reflect.Method;
 
 @Controller
 @RequestMapping("/customer")
@@ -33,9 +37,20 @@ public class CustomerController {
     }
 
     @PostMapping("/write")
-    public String writeProc(CustomerEntity entity) {
-        int result = service.insCustomer(entity);
-        return "redirect:/customer/list/" + entity.getBoard_cd();
+    public String writeProc(CustomerEntity entity, MultipartFile[] files, Model model) {
+
+//        try {
+            boolean isRegistered = this.service.insCustomer(entity, files);
+//            if (!isRegistered) {
+//                return this.showMessageWithRedirect("게시글 등록에 실패하였습니다.", "/board/list.do", Method.GET, pagingParams, model);
+//            }
+//        } catch (DataAccessException var6) {
+//            return this.showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.", "/board/list.do", Method.GET, pagingParams, model);
+//        } catch (Exception var7) {
+//            return this.showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list.do", Method.GET, pagingParams, model);
+//        }
+
+        return "redirect:/customer/list" + entity.getBoard_cd();
     }
 
     @GetMapping("/detail")
---
+++ src/main/java/com/portfolio/lagarto/customer/files/AttachFileException.java
@@ -0,0 +1,13 @@
+package com.portfolio.lagarto.customer.files;
+
+@SuppressWarnings("serial")
+public class AttachFileException extends RuntimeException {
+    public AttachFileException(String message) {
+        super(message);
+    }
+
+    public AttachFileException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
